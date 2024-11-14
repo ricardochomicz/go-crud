25990db12a54662fdfa1de8aa1fdf6ee3e90bb0b
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-11-14 16:27:24 -0300

    refactor: validates request

--- src/model/request/user_request.go
+++ src/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Age      int8   `json:"age"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,max=16"`
+	Name     string `json:"name" binding:"required,min=4,max=50"`
+	Age      int8   `json:"age" binding:"required"`
 }
