f2f9ecdb91bc9dd50b31a5afed140ce32d1cf3db
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-11-14 16:26:54 -0300

    refactor: update controller user

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,23 +1,40 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/ricardochomicz/go-crud/src/configuration/rest_err"
+	"github.com/ricardochomicz/go-crud/src/configuration/logger"
+	"github.com/ricardochomicz/go-crud/src/configuration/validation"
 	"github.com/ricardochomicz/go-crud/src/model/request"
+	"github.com/ricardochomicz/go-crud/src/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
-
+	logger.Info("Init create user controller",
+		zap.String("journey", "createUser"),
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("invalid json body: %s\n", err.Error()),
-		)
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"))
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
+
+	response := response.UserResponse{
+		ID:    "Teste",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	logger.Info("Create user successfully",
+		zap.String("journey", "createUser"))
+
+	c.JSON(http.StatusOK, response)
 
 }
