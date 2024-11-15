601d137565a720079c3cc8f021ada34b24eb3320
Ricardo <ricardo.chomicz@gmail.com>
2024-11-14 22:48:53 -0300

    feat: user domain interface

---
+++ src/model/user_domain_interface.go
@@ -0,0 +1,23 @@
+package model
+
+type UserDomainInterface interface {
+	GetEmail() string
+	GetPassword() string
+	GetName() string
+	GetAge() int8
+	GetJSONValue() (string, error)
+	SetID(string)
+	EncryptPassword()
+}
+
+func NewUserDomain(
+	email, password, name string,
+	age int8,
+) UserDomainInterface {
+	return &userDomain{
+		email:    email,
+		password: password,
+		name:     name,
+		age:      age,
+	}
+}
