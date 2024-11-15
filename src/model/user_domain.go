befc7c77b63413dcee2461b0e58ce22e4fbd1f83
Ricardo <ricardo.chomicz@gmail.com>
2024-11-14 22:49:52 -0300

    fix: user domain updates

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -1,44 +1,20 @@
 package model
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 )
 
-type UserDomainInterface interface {
-	GetEmail() string
-	GetPassword() string
-	GetName() string
-	GetAge() int8
-	GetJSONValue() (string, error)
-	SetID(string)
-	EncryptPassword()
-}
-
-func NewUserDomain(
-	email, password, name string,
-	age int8,
-) UserDomainInterface {
-	return &userDomain{
-		Email:    email,
-		Password: password,
-		Name:     name,
-		Age:      age,
-	}
-}
-
 func (ud *userDomain) SetId(id string) {
-	ud.ID = id
+	ud.id = id
 }
 
 type userDomain struct {
-	ID       string `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Age      int8   `json:"age"`
+	id       string
+	email    string
+	password string
+	name     string
+	age      int8
 }
 
 func (ud *userDomain) GetJSONValue() (string, error) {
@@ -51,33 +27,22 @@ func (ud *userDomain) GetJSONValue() (string, error) {
 }
 
 // SetID implements UserDomainInterface.
-func (ud *userDomain) SetID(string) {
-	panic("unimplemented")
+func (ud *userDomain) SetID(id string) {
+
 }
 
 func (ud *userDomain) GetEmail() string {
-	return ud.Email
+	return ud.email
 }
 func (ud *userDomain) GetPassword() string {
-	return ud.Password
+	return ud.password
 }
 func (ud *userDomain) GetName() string {
-	return ud.Name
+	return ud.name
 }
 func (ud *userDomain) GetAge() int8 {
-	return ud.Age
+	return ud.age
 }
 
 // EncryptPassword é um método da estrutura UserDomain que tem como objetivo
 // criptografar a senha do usuário utilizando o algoritmo MD5.
-func (ud *userDomain) EncryptPassword() {
-	// Cria um novo hash MD5.
-	hash := md5.New()
-	// Garante que o hash será redefinido após o uso, evitando vazamentos de memória.
-	defer hash.Reset()
-	// Escreve a senha do usuário no hash. A senha é convertida para bytes.
-	hash.Write([]byte(ud.Password))
-	// Calcula o hash e converte o resultado para uma string hexadecimal.
-	// O resultado é então atribuído de volta à propriedade Password do usuário.
-	ud.Password = hex.EncodeToString(hash.Sum(nil))
-}
