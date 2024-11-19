c449c44d0c1659971907718ab58308c583807d9d
Ricardo <ricardo.chomicz@gmail.com>
2024-11-19 19:55:53 -0300

    fix: update user

---
+++ src/model/repository/update_user_repository.go
@@ -0,0 +1,47 @@
+package repository
+
+import (
+	"context"
+	"os"
+
+	"github.com/ricardochomicz/go-crud/src/configuration/logger"
+	"github.com/ricardochomicz/go-crud/src/configuration/rest_err"
+	"github.com/ricardochomicz/go-crud/src/model"
+	"github.com/ricardochomicz/go-crud/src/model/repository/entity/converter"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/zap"
+)
+
+func (ur *userRepository) UpdateUser(
+	userId string,
+	userDomain model.UserDomainInterface,
+) *rest_err.RestErr {
+
+	logger.Info("Init updateUser repository")
+
+	collection_name := os.Getenv(MONGODB_COLLECTION)
+
+	collection := ur.databaseConnection.Collection(collection_name)
+
+	value := converter.ConverterDomainEntity(userDomain)
+
+	userIdHex, _ := primitive.ObjectIDFromHex(userId)
+
+	filter := bson.D{{Key: "_id", Value: userIdHex}}
+	update := bson.D{{Key: "$set", Value: value}}
+
+	_, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		logger.Error("Error trying to update user",
+			err,
+			zap.String("journey", "updateUser"))
+		return rest_err.NewInternalServerError(err.Error())
+	}
+
+	logger.Info("Update repository executed successfully",
+		zap.String("userId", userId),
+		zap.String("journey", "updateUser"))
+
+	return nil
+}
