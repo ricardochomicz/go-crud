d690a3c3223230afa3d06ce6b6652475c1cb5899
Ricardo <ricardo.chomicz@gmail.com>
2024-11-19 19:55:31 -0300

    feat: update user

--- src/controller/update_user.go
+++ src/controller/update_user.go
@@ -1,5 +1,62 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func (uc *userControllerInterface) UpdateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/ricardochomicz/go-crud/src/configuration/logger"
+	"github.com/ricardochomicz/go-crud/src/configuration/validation"
+	"github.com/ricardochomicz/go-crud/src/model"
+	"github.com/ricardochomicz/go-crud/src/model/request"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/zap"
+)
+
+func (uc *userControllerInterface) UpdateUser(c *gin.Context) {
+	logger.Info("Init update user controller",
+		zap.String("journey", "updateUser"),
+	)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "updateUser"))
+		restErr := validation.ValidateUserError(err)
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	userId := c.Param("userId")
+	if _, err := primitive.ObjectIDFromHex(userId); err != nil {
+		logger.Error("Invalid userId", err,
+			zap.String("journey", "updateUser"))
+		restErr := validation.ValidateUserError(err)
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	domain := model.UpdateUserDomain(
+		userRequest.Name,
+		userRequest.Age,
+	)
+
+	err := uc.service.UpdateUserService(userId, domain)
+	if err != nil {
+		c.JSON(err.Code, err)
+		return
+	}
+
+	// response := response.UserResponse{
+	// 	ID:    "Teste",
+	// 	Email: userRequest.Email,
+	// 	Name:  userRequest.Name,
+	// 	Age:   userRequest.Age,
+	// }
+
+	logger.Info("Update user successfully",
+		zap.String("userId", userId),
+		zap.String("journey", "updateUser"))
+
+	c.Status(http.StatusOK)
+
+}
